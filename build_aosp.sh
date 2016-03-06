22e1c06118bc23a070e66a9415eab83a817caf10
Maxime Poulain <bosstamam@gmail.com>
2016-03-06 16:33:04 +0100

    fix an issue when the build_aosp.sh script was called outside its folder

--- build_aosp.sh
+++ build_aosp.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+build_scripts_dir=/home/khaon/android/rom/build_scripts;
+
 # The root folder of your AOSP
 android_root_path=/home/khaon/android/rom/aosp_master;
 android_out_folder=~/out/aosp_master;
@@ -9,11 +11,11 @@ device_lunch_number=25;
 rm -rf $android_out_folder;
 
 #Update our personal projects before repo syncing
-sh update_personnal_projects.sh;
+sh $build_scripts_dir/update_personnal_projects.sh;
 
 cd $android_root_path;
 
 repo sync -j5 --force-sync -f;
-. build/envsetup.sh
+source build/envsetup.sh
 lunch $device_lunch_number;
 make -j$(cat /proc/cpuinfo | grep processor | wc -l) otapackage;
